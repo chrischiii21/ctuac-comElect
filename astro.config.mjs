7ba135d549c653b09bd82e7bb3be6db61a15fe89
Christy Montejo <christymontejo2003@gmail.com>
2026-05-12 18:45:37 +0800

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel';
+import netlify from '@astrojs/netlify';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
+  adapter: netlify(),
   integrations: [
     react(),
     tailwind({
